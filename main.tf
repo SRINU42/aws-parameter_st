8e1325f8f39630302672fad1498b7cae96519271
SRINU42 <srinu.karnam01@gmail.com>
2025-01-30 11:34:37 +0530

    count update

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 resource "aws_ssm_parameter" "param" {
-  count = length{var.param}
+  count = length(var.param)
   name  = var.param[count.index].name
   type  = var.param[count.index].type
   value = var.param[count.index].value
