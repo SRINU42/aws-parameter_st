e68dad4fa1051ad2eea17388ecde56e021a6cedc
SRINU42 <srinu.karnam01@gmail.com>
2025-01-30 10:52:09 +0530

    param

---
+++ main.tf
@@ -0,0 +1,13 @@
+resource "aws_ssm_parameter" "param" {
+  count = length{var.param}
+  name  = var.param[count.index].name
+  type  = var.param[count.index].type
+  value = var.param[count.index].value
+}
+
+variabale "param" {
+    default = {
+        {name = "demo1",value = "demo1", type = "string"} ,
+        {name = "demo2",value = "demo2", type = "string"}
+    }
+}
