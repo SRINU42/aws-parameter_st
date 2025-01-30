964d97adc0a78a5bf5969bdbe2053f7f3a1253e9
SRINU42 <srinu.karnam01@gmail.com>
2025-01-30 16:19:51 +0530

    update

--- main.tf
+++ main.tf
@@ -10,7 +10,7 @@ variable "param" {
         { name = "roboshop.dev.frontend.catalogue_url", value = "http://catalogue-dev.erobo.online:8080/", type = "String" },
         { name = "roboshop.dev.frontend.user_url", value = "http://user-dev.erobo.online:8080/", type = "String" },
         { name = "roboshop.dev.frontend.cart_url", value = "http://cart-dev.erobo.online:8080/", type = "String" },
-        { name = "roboshop.dev.frontend.payment", value = "http://payment-dev.erobo.online:8080/", type = "String" },
+        { name = "roboshop.dev.frontend.payment_url", value = "http://payment-dev.erobo.online:8080/", type = "String" },
         { name = "roboshop.dev.frontend.shipping_url", value = "http://shipping-dev.erobo.online:8080/", type = "String" }
 
         
